94e10c526cff43a7f6064b20847a2c8b999b5361
Talha Ansari <154464182+TalhaAnsari322@users.noreply.github.com>
2024-03-28 02:55:32 +0500

    Add files via upload

---
+++ arrey practice/app.js
@@ -0,0 +1,62 @@
+//      CHAPTER 12 TO 16
+//     --- Question 1 ---
+
+// var futureStudents = [];
+// document.write(`Future Students: ${futureStudents}`);
+
+
+//     --- Question 2 ---
+
+// var futureStudents = [];
+// futureStudents.push("Muhammad");
+// futureStudents.push("Talha");
+// futureStudents.push("Ansari");
+// document.write(futureStudents);
+
+//     --- Question 3 ---
+
+// var stringsArray = ["Muhammad", "Talha" , "Ansari"]
+// document.write(`Strings Array: ${stringsArray}`)
+
+//     --- Question 4 ---
+
+// var numbersArray = [1,2,3,4]
+// document.write(`Numbers Arrays: ${numbersArray}`)
+
+//     --- Question 5 ---
+
+// var booleanArrays = [true, false]
+// document.write(`Boolean Arrays: ${booleanArrays}`)
+
+
+//     --- Question 6 ---
+
+// var mixedArrays = ["Talha", 22 , "SMIT", true , false , 3.20 ]
+// document.write(`<h1>Mixed Arrays: ${mixedArrays}</h1> <br> My Name is ${mixedArrays[0]} <br> I'm ${mixedArrays[1]} Years Old <br> JS Boolean values are ${mixedArrays[3]} & ${mixedArrays[4]} <br> Numeric Value is ${mixedArrays[5]}` )
+
+//     --- Question 7 ---
+
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil" , "PhD"]
+// document.write(`<h1>QUALIFICATIONS:</h1> 1) ${qualifications[0]} <br>2) ${qualifications[1]} <br>3) ${qualifications[2]} <br>4) ${qualifications[3]} <br> 5) ${qualifications[4]} <br> 6) ${qualifications[5]} <br>7) ${qualifications[6]} <br>8) ${qualifications[7]} <br>  `)
+
+
+//     --- Question 8 ---
+
+// var studentNames = ["Muhammad", "Talha", "Ansari"]
+// var studentMarks = [320, 230, 480]
+// var totalMarks = 500;
+
+// var anasTotal = studentMarks[0]/totalMarks*100
+// var areebTotal = studentMarks[1]/totalMarks*100
+// var shayanTotal = studentMarks[2]/totalMarks*100
+
+
+// document.write(`Score of Anas is ${studentMarks[0]}. Percentage: ${talhaTotal}% <br> Score of Muhammad is ${studentMarks[1]}. Percentage: ${MuhammadTotal}% <br> Score of Ansari is ${studentMarks[2]}. Percentage: ${talhaTotal}%`)
+
+//     --- Question 9---
+
+// var colors = ["Red","Green"]
+
+
+
+
